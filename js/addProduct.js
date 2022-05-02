b77ddd0b53e60b632ec842cd8f02729efa1fa02c
horex-Pro <78277303+horex-Pro@users.noreply.github.com>
2022-05-02 14:19:46 +0430

    test

---
+++ js/addProduct.js
@@ -0,0 +1,62 @@
+export const productsData = [
+    {
+        id:1,
+        name:"Nike Model 11005",
+        price:2500.00,
+        imageUrl:"./images/1.png",
+    },
+    {
+        id:2,
+        name:"Moje 15-007",
+        price:2120.00,
+        imageUrl:"./images/2.png"
+    },
+    {
+        id:3,
+        name:"Nike Model 12788",
+        price:1100.00,
+        imageUrl:"./images/3.png"
+    },
+    {
+        id:4,
+        name:"Nike Model 11005",
+        price:3000.00,
+        imageUrl:"./images/4.png"
+    },
+    {
+        id:5,
+        name:"Salamon a1542",
+        price:3042.00,
+        imageUrl:"./images/5.png"
+    },
+    {
+        id:6,
+        name:"Nike Model 11005",
+        price:3000.00,
+        imageUrl:"./images/6.png"
+    },
+    {
+        id:7,
+        name:"Nike Model 5481",
+        price:1800.00,
+        imageUrl:"./images/1.png"
+    },
+    {
+        id:8,
+        name:"Aria Model 12s",
+        price:1853.00,
+        imageUrl:"./images/2.png"
+    },
+    {
+        id:9,
+        name:"Aria Model 12s",
+        price:1853.00,
+        imageUrl:"./images/2.png"
+    },
+    {
+        id:10,
+        name:"tantak shahin2",
+        price:2.00,
+        imageUrl:"./images/3.png"
+    },
+]
--- js/app.js
+++ js/app.js
@@ -2,9 +2,23 @@ const modalContainer = document.querySelector('.modal-container');
 const basketBtn = document.querySelector('#basketBtn');
 const closeModalBtn = document.querySelector('#closeModalBtn');
 
-basketBtn.addEventListener('click',(e)=>{
-    modalContainer.style.display = 'flex';
-})
-closeModalBtn.addEventListener('click',(e)=>{
-    modalContainer.style.display = 'none'
+basketBtn.addEventListener('click',(e) => modalContainer.style.display = 'flex');
+closeModalBtn.addEventListener('click',(e) => modalContainer.style.display = 'none');
+
+
+import { productsData } from "./addProduct.js";
+
+class Products {
+    getProduct(){
+        return productsData;
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const products = new Products();
+    const productsData = products.getProduct();
+
+    console.log(productsData);
 })
